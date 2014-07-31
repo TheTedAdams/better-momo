c622584d0d3b5fc6d8a7422a85985ddd3dc7a338
tadams88 <t.adams88@gmail.com>
2014-07-30 17:59:15 -0700

    Setting up plumbing

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-Add extended functionality to MOMO.
+```javascript
+javascript:(function(a){var b=a.createElement("script");b.src="https://raw.github.com/tadams88/better-momo/master/src/betterMomo.js";a.getElementsByTagName("head")[0].appendChild(b)})(document);
+```
+
+Save this <a href="http://en.wikipedia.org/wiki/Bookmarklet">bookmarklet</a> to your browser to add custom improvements to MOMO.
+
+Current features:
+
+ - Add Time Tracking functionality
---
+++ src/betterMomo.js
@@ -0,0 +1,41 @@
+(function() {
+	var activeTimer = 0;
+
+	var css = ".js-trackTime { cursor:pointer;display:inline-block;vertical-align:middle;width:12px;height:12px;background: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAwAAAAMCAYAAABWdVznAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAABxSURBVChTY6AE+EAx0SABiP8DcT8Qc4AECAGYBhA+D8QaQIwBmKA0OjAA4tNAnALm4QDINiDj5UAsAMRggMsGZCADxERp+APELUDsCMQPQALoANlJj4HYAYgxADYbNgCxIRAfAPPwgAggzoEwqQYYGACqahYQId2KYAAAAABJRU5ErkJggg==') no-repeat center center }" +
+			  ".js-trackTime.active { background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAwAAAAMCAYAAABWdVznAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAjSURBVChTYxj8YD8Q/wfiBCgGsUFicMAEpYkGw0HDoAMMDADrZgRHMFH1hQAAAABJRU5ErkJggg=='); }";
+
+    var head = document.head || document.getElementsByTagName('head')[0],
+    	style = document.createElement('style');
+	style.type = 'text/css';
+	if (style.styleSheet)
+		style.styleSheet.cssText = css;
+	else
+		style.appendChild(document.createTextNode(css));
+	head.appendChild(style);
+
+	$('input[type=text]').each(function() {
+		var $t = $(this),
+			$e = $('<span class="js-trackTime"></span>');
+
+		$e.data('timeInput', this);
+		$t.closest('td.Time').append($e);
+	});
+
+	$('.js-trackTime').click(function(e) {
+		var $t = $(this),
+			$i = $($t.data('timeInput'));
+		window.clearInterval(activeTimer);
+		if($t.hasClass('active')) {
+			$t.removeClass('active');
+			$i.focus().blur();
+			return;
+		}
+		var $a = $('.js-trackTime.active');
+		$($a.data('timeInput')).focus().blur();
+		$a.removeClass('active');
+		$t.addClass('active');
+		activeTimer = window.setInterval(function() {
+			$i.val(+$i.val() + 1);
+		}, 360000);
+	});
+})();
